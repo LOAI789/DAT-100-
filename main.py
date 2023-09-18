1c087d3ce07623a677213f4553aafa89d7d97684
Christian Tran <christian.tran@student.uib.no>
2023-09-18 22:21:57 +0200

    Oppgave d og i

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import lister_for_del_1
+
+
 
 #d 
 def noe(liste,tall):
+    sum = 0
     for i in liste:
-        if tall >= i:
+        if i >= tall:
             sum +=1
     return sum
 
 #i
+print("Antall dager over eller lik 20:",noe(lister_for_del_1.temperaturer,20))
+print("Antall dager over eller lik 25:",noe(lister_for_del_1.temperaturer,25))
+print("Antall dager over eller lik 30:",noe(lister_for_del_1.temperaturer,30))
