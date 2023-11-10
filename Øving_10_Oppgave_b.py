af2ef8dca64d50bd876f496bbd54d591ae937244
Christian Tran <christian.tran@student.uib.no>
2023-11-10 11:57:19 +0100

    ok

--- Øving_10_Oppgave_b.py
+++ Øving_10_Oppgave_b.py
@@ -1,3 +1,7 @@
-
 from Øving_10_Oppgave_a import les_værdata
 
+fil = "snoedybder_vaer_en_stasjon_dogn.csv"
+
+#Sesong = 1 Okt - 1 Juni (1.10 - 1.6)
+værdata = les_værdata(fil)
+print(værdata[1]["Dato"])
